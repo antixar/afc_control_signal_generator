b52469e8d39fc0bda8833292a5eb64c3eb0fb5bc
antixar <antixar@gmail.com>
2023-01-25 21:38:14 +0200

    add words

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# afc_control_signal_generator
+# Simple generator for updating of code tables
+
+This is easy script that can generate random signals with Ukrainian words
+Examples signals: ВОВК-123, ЧЕРЕШНЯ-3443 etc
+
+It has 3 required arguments:
+-s -- start value of digit part. As rule as this part is used for fast searching
+-w -- type of words. You can select needed files with words
+-c -- count of generated combination
+
+The script will return a digit value(without index part) if a word is digit value
---
+++ control_signal_generator/__main__.py
@@ -0,0 +1,46 @@
+import argparse
+import os, sys
+from pathlib import Path
+import random
+
+def read_bases():
+    folder_path = Path(__file__).parent.parent / "words"
+    if not folder_path.is_dir():
+        raise SystemExit("Not found a folder with words!!!")
+    files = {}
+    for f in folder_path.iterdir():
+        if f.is_file() and str(f).endswith(".txt"):
+            files[f.name.replace(".txt", "")] = str(f)
+    return files
+
+
+def main():
+    words = read_bases()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-s', '--start', required=True,nargs='?', help='start signal index', type=int)
+    parser.add_argument('-c', '--count', required=True, nargs='?', help='limit generated signals', type=int)
+    parser.add_argument('-w', '--word', required=True, action='append', nargs='?', choices=words.keys(), help='word types', type=str)
+
+    args = parser.parse_args()
+    for signal in generate(args.start, args.count, [v for k, v in words.items() if k in args.word]):
+        print(signal)
+
+
+def generate(start_digit, count, files):
+    words = []
+    for f in files:
+        with open(f) as ff:
+            words += [line.strip() for line in ff]
+    if count > len(words):
+        raise SystemExit("There are words less than needed limit")
+    random.shuffle(words)
+
+    for i in range(count):
+        word =words[i]
+        if not word.isdigit():
+            word += "-" + str(start_digit + i)
+        yield word.upper()
+
+
+
+main()
